94443caf8d74776d3c70ab7d29342d6406b7122b
Stina <stinaahlstroem@gmail.com>
2020-02-19 20:56:29 +0100

    added schools data

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -1,10 +1,44 @@
 /**
  * A function to wrap it all in.
  */
-(function () {
-    "use strict";
 
+function loadSchools() {
+    'use strict';
 
+    let url;
 
-    console.log("All ready.");
-}());
+    //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1283";
+    url = "data/schools.json";
+
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            populate(myJson);
+        });
+};
+// create table rows and populate them with data
+
+function populate(data) {
+    var table = document.getElementById("schools");
+    table.style.display="block";
+    let i=1;
+  
+    data.Skolenheter.forEach(element => {
+        
+        var row = table.insertRow(i);
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+        cell1.innerHTML = element.Skolenhetskod;
+        cell2.innerHTML = element.Skolenhetsnamn;
+        cell3.innerHTML = element.Kommunkod;
+        cell4.innerHTML = element.PeOrgNr;
+
+        i++;
+    }); 
+}
