8c854b5113bce8ae7a85cdd53bcb2a1ca043daff
Stina <stinaahlstroem@gmail.com>
2020-02-23 14:18:06 +0100

    s04 preparations

---
+++ work/s04/node/hello2.js
@@ -0,0 +1,34 @@
+/**
+ * A sample of a main function stating the famous Hello World.
+ *
+ * @returns void
+ */
+"use strict";
+
+function main() {
+    let a = 1;
+    let b;
+    let range = "";
+
+    b = a + 20;
+
+    for (let i=0; i < 9; i++) {
+        range += i + ", ";
+    }
+
+    while(a < b) {
+        if(a % 3 ==0) {
+            console.info(a)
+
+        } 
+        a++;
+    }
+
+    console.info("Hello World");
+    console.info(range.substring(0, range.length - 2));
+    console.info(a, b);
+    console.info(new Date().toLocaleString());
+}
+
+main();
+
